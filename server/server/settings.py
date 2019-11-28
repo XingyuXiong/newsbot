8a0b7149db4384bbea208dcda7654da2842d2b1f
Xingyu Xiong <peter__xiong@163.com>
2019-11-28 17:48:19 +0800

    add django fixtures

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'server'
+    'state'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FIXTURE_DIRS = ('/path/to/api/fixtures/',)
